a5048dfd8562ece4ee4db9bfc2b5cebf44252eba
estudioabraaotattoo07-cell <estudioabraaotattoo07@gmail.com>
2026-06-01 21:32:52 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 import CRM from './CRM Casa dos Carvalho'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
